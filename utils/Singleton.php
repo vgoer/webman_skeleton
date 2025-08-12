cd0db3b6ba6f10b1b9ed5574d7a3e95d81f8cd3b
vgoer <goerblue@gmail.com>
2025-08-12 12:48:24 +0800

    feat: 添加Singleton单列的工具类

---
+++ utils/Singleton.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace utils;
+
+trait Singleton
+{
+    private static $instance;
+
+    /**
+     * get instance
+     *
+     * @param mixed ...$args
+     *
+     * @return static
+     */
+    static function instance(...$args)
+    {
+        $className = md5(get_called_class() . serialize($args));
+        if(!isset(self::$instance[$className])){
+            self::$instance[$className] = new static(...$args);
+        }
+        return self::$instance[$className];
+    }
+}
